fcbf4f82ebee558bac7e7063270f4aae34c4eabf
lirilabs <lirilabs@gmail.com>
2025-12-14 22:41:35 +0530

    Update id-map.js

--- api/_debug/id-map.js
+++ api/_debug/id-map.js
@@ -3,10 +3,17 @@ import { readJSON } from "../_lib/github.js";
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  const result = await readJSON("data/indexes/id-map.json");
+  try {
+    const result = await readJSON("data/indexes/id-map.json");
 
-  return res.json({
-    exists: !!result.json,
-    data: result.json
-  });
+    return res.json({
+      exists: !!result.json,
+      data: result.json
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: "Debug error",
+      detail: err.message
+    });
+  }
 }
