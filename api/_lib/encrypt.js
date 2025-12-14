897eeabdb76979baa3283a1144061a7a4490b24f
lirilabs <lirilabs@gmail.com>
2025-12-14 22:31:39 +0530

    Update encrypt.js

--- api/_lib/encrypt.js
+++ api/_lib/encrypt.js
@@ -3,14 +3,17 @@ import crypto from "crypto";
 const ALGO = "aes-256-gcm";
 const KEY = Buffer.from(process.env.ENCRYPTION_KEY, "utf8");
 
-export function encrypt(text) {
-  const iv = crypto.randomBytes(12);
-  const cipher = crypto.createCipheriv(ALGO, KEY, iv);
+export function decrypt(payload) {
+  const [ivB64, tagB64, encrypted] = payload.split(".");
+  const iv = Buffer.from(ivB64, "base64");
+  const tag = Buffer.from(tagB64, "base64");
 
-  let encrypted = cipher.update(text, "utf8", "base64");
-  encrypted += cipher.final("base64");
+  const decipher = crypto.createDecipheriv(ALGO, KEY, iv);
+  decipher.setAuthTag(tag);
 
-  const tag = cipher.getAuthTag().toString("base64");
+  let text = decipher.update(encrypted, "base64", "utf8");
+  text += decipher.final("utf8");
 
-  return `${iv.toString("base64")}.${tag}.${encrypted}`;
+  return text;
 }
+
